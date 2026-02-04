58c236bb13a0881256a9c57f9376fc82640eebfe
dariober <dario.beraldi@gmail.com>
2026-02-04 12:34:50 +0000

    Require Java >= 17

--- Formula/asciigenome.rb
+++ Formula/asciigenome.rb
@@ -4,14 +4,13 @@ class Asciigenome < Formula
   url "https://github.com/dariober/ASCIIGenome/releases/download/v1.20.0/ASCIIGenome-1.20.0.zip"
   sha256 "8fce70d7e555c67e4fb27a99763e41a18d91a9a596695f22ce233c7a26cfc02f" 
 
+  depends_on "openjdk@17"
+
   def install
     jar = "ASCIIGenome.jar"
     java = share/"java"
     java.install jar
     bin.write_jar_script java/jar, "ASCIIGenome"
-
-    inreplace("ASCIIGenome", /^prefix.*$/, "prefix=#{java}")
-    bin.install %w[ASCIIGenome]
   end
 
   test do
